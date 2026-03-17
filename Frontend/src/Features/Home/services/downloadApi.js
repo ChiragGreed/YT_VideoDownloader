e5a516e5a4d2aedd5dd3f76e82f5988d3289e272
Chirag Greed <chiraggreed02@gmail.com>
2026-03-17 22:31:52 +0530

    Route typo

--- Frontend/src/Features/Home/services/downloadApi.js
+++ Frontend/src/Features/Home/services/downloadApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://yt-videodownloader-chfj.onrender.com',
+    baseURL: 'https://yt-videodownloader-chfj.onrender.com/api',
     withCredentials: true
 })
 
